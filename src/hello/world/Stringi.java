f5018f167e51c8f36ac6fc0c7025557bc4e38923
HIKOTIH <hikotih82@gmail.com>
2022-09-07 20:43:02 +0300

    l2

---
+++ src/hello/world/Stringi.java
@@ -0,0 +1,13 @@
+package hello.world;
+
+public class Stringi {
+    public static void main(String[] args) {
+        int x = 5;
+        String d = "String";
+        String f = " - ";
+        String g = "class";
+
+        System.out.println(d+f+g);
+
+    }
+}
