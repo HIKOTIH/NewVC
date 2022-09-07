4e62666d37f7c6da18e661e9da48a4c344e2cfa4
HIKOTIH <hikotih82@gmail.com>
2022-09-07 20:22:36 +0300

    l2

---
+++ src/hello/world/Variables.java
@@ -0,0 +1,20 @@
+package hello.world;
+
+public class Variables {
+    public static void main(String[] args) {
+        int newInt = 2525;
+
+        short newShort = 3434;
+        long newLng = 25252525;
+
+        char a = 'b';
+        boolean b = true;
+
+        double newDouble = 432.22;
+        float newFloat = 432.22f;
+
+        byte newByte = 127; // -128 127
+
+        System.out.println(newLng);
+    }
+}
