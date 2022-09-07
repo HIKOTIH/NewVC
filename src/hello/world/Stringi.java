48925cc317a33582e0159e5df20a29ee995ee419
HIKOTIH <hikotih82@gmail.com>
2022-09-07 20:44:30 +0300

    l2

--- src/hello/world/Stringi.java
+++ src/hello/world/Stringi.java
@@ -4,7 +4,7 @@ public class Stringi {
     public static void main(String[] args) {
         int x = 5;
         String d = "String";
-        String f = " - ";
+        String f = " = ";
         String g = "class";
 
         System.out.println(d+f+g);
