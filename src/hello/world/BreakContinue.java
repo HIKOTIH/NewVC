e90dcb2dae8ddee2e21bc68d3eb8686dc474e325
HIKOTIH <hikotih82@gmail.com>
2022-09-08 20:51:56 +0300

    dw2

---
+++ src/hello/world/BreakContinue.java
@@ -0,0 +1,16 @@
+package hello.world;
+
+public class BreakContinue {
+    public static void main(String[] args) {
+        int i=0;
+       while (true){
+           if(i==15){
+               break;
+           }
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Цыкл стоп");
+    }
+
+}
