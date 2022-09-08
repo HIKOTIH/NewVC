61e868b22dc146da2b4ed653bf65ffa0ef7391a3
HIKOTIH <hikotih82@gmail.com>
2022-09-08 19:33:27 +0300

    l4

---
+++ src/hello/world/If.java
@@ -0,0 +1,15 @@
+package hello.world;
+
+public class If {
+    public static void main(String[] args) {
+        int myInt = 15;
+        if (myInt < 10) {
+            System.out.println("красава");
+        } else if (myInt > 20) {
+            System.out.println("переделай");
+        } else {
+            System.out.println("mda");
+        }
+    }
+}
+
