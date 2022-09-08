59fd43e6056204cb43da5f4f468505551543fd00
HIKOTIH <hikotih82@gmail.com>
2022-09-08 19:24:51 +0300

    l4

---
+++ src/hello/world/ForLoop.java
@@ -0,0 +1,9 @@
+package hello.world;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i = i+5)
+            System.out.println("Ky" + i);
+
+    }
+}
