f099a7b81ea519fb928ef51d60616c07b7d90726
HIKOTIH <hikotih82@gmail.com>
2022-09-08 19:15:37 +0300

    l4

--- src/hello/world/WhileLoops.java
+++ src/hello/world/WhileLoops.java
@@ -2,8 +2,11 @@ package hello.world;
 
 public class WhileLoops {
     public static void main(String[] args) {
-      boolean t= true;
-        System.out.println(t);
+        int value = 0;
+        while (value < 10){
+            System.out.println("Number" + value);
+        value = value + 3;
+        }
     }
 
 }
